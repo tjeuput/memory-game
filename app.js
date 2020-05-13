acb2ff30a27ef8b7a8de7f201b28fd53cc42a8c7
tjeuput <fucdce@gmail.com>
2020-05-13 09:23:46 +0200

    E6 function on shuffling the cards

--- app.js
+++ app.js
@@ -69,29 +69,17 @@ let cardArray = [
 ]
 
 //randomize card function
-function shuffle(array) {
-	var currentIndex = array.length, temporaryValue, randomIndex;
-  
-	// While there remain elements to shuffle...
-	while (0 !== currentIndex) {
-  
-	  // Pick a remaining element...
-	  randomIndex = Math.floor(Math.random() * currentIndex);
-	  currentIndex -= 1;
-  
-	  // And swap it with the current element.
-	  temporaryValue = array[currentIndex];
-	  array[currentIndex] = array[randomIndex];
-	  array[randomIndex] = temporaryValue;
-	}
-	console.log(currentIndex)
-	return array;
-	
-  }
-  
-  // Used like so
+function shuffleCard(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+		[array[i], array[j]] = [array[j], array[i]];
 
-cardArray = shuffle(cardArray)
+	}
+	
+	return array
+}
+
+cardArray = shuffleCard(cardArray)
 cardArray.sort()
 
 
@@ -121,8 +109,7 @@ function allPicture(){
 	for(let i = 0; i < cardArray.length; i++){
 		var card = document.createElement('img')
 		card.src = cardArray[i].img
-		gpic.appendChild(card)
-		
+		gpic.appendChild(card)	
 		
 	} 
 }
